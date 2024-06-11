476ab946cdd197dfa7ec5d6a14c8d095a830faa6
mmahmudul7 <hasanitgroup@gmail.com>
2024-06-11 17:19:18 +0600

    1074

--- BeeCrowd/BEGINNER/1073.cpp
+++ BeeCrowd/BEGINNER/1073.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main(){
     int n;
     cin >> n;
 
     for (int i = 1; i <= n; i++)
-    {
         if (i % 2 == 0)
-        {
-            int sqr = i * i;
-
-            cout << i << "^" << "2" << " = " << sqr << endl;
-        }
-    }
+            cout << i << "^" << "2 = " << i * i << endl;
     return 0;
 }
