9382d90c6ce86b3904760c8ea4f5ea14a86393c0
mmahmudul7 <hasanitgroup@gmail.com>
2024-06-12 20:34:08 +0600

    1075 and module 2

--- BeeCrowd/BEGINNER/1074.cpp
+++ BeeCrowd/BEGINNER/1074.cpp
@@ -5,8 +5,7 @@ int main(){
     int n, x;
 
     cin >> n;
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         cin >> x;
         if (x == 0)
             cout << "NULL\n";
