9df8e4092fc2bdff9443bbc10ca97e2405201580
mmahmudul7 <hasanitgroup@gmail.com>
2024-06-13 21:50:52 +0600

    1078

---
+++ BeeCrowd/BEGINNER/1078.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    
+    for(int i = 1; i <= 10; i++)
+        cout << i << " x " << N << " = " << N * i << endl;
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+ 
+    return 0;
+}
