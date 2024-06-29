d693b862ca2d71db0c520c4564a41fd3e1700ba4
mmahmudul7 <hasanitgroup@gmail.com>
2024-06-29 23:49:20 +0600

    1131 - Grenais

---
+++ BeeCrowd/BEGINNER/1131.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+    int op;
+    int inter = 0;
+    int gremio = 0;
+    int grenal = 0;
+    int empate = 0;
+
+    while (true)
+    {
+        cin >> x;
+        cin >> y;
+
+        if (x > y)
+            inter++;
+        if (x < y)
+            gremio++;
+        if (x == y)
+            empate++;
+        grenal++;
+
+        cout << "Novo grenal (1-sim 2-nao)\n";
+        cin >> op;
+
+        if (op == 2)
+            break;
+    }
+    cout << grenal << " grenais" << endl;
+    cout << "Inter:" << inter << endl;
+    cout << "Gremio:" << gremio << endl;
+    cout << "Empates:" << empate << endl;
+
+    if (inter > gremendl)
+        cout << "Inter venceu mais" << endl;
+    if (inter < gremio)
+        cout << "Gremio venceu mais" << endl;
+    if (inter == gremio)
+        cout << "Nao houve vencedor" << endl;
+
+    return 0;
+}
