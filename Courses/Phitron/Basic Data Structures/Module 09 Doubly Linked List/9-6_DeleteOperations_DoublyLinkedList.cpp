76a27c20271237f7417eca8b3e4c33488825c70a
mmahmudul7 <hasanitgroup@gmail.com>
2024-07-01 12:38:40 +0600

    Delete at position head tail

---
+++ Courses/Phitron/Basic Data Structures/Module 09 Doubly Linked List/9-6_DeleteOperations_DoublyLinkedList.cpp
@@ -0,0 +1,135 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    Node *prev;
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->prev = NULL;
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+int size(Node *head)
+{
+    Node *tmp = head;
+    int cnt = 0;
+    while (tmp != NULL)
+    {
+        cnt++;
+        tmp = tmp->next;
+    }
+    return cnt;
+}
+
+void delete_head(Node *&head, Node *&tail)
+{
+    Node *deleteNode = head;
+    head = head->next;
+    delete deleteNode;
+    if (head == NULL)
+    {
+        tail = NULL;
+        return;
+    }
+    head->prev = NULL;
+}
+
+void delete_at_position(Node *head, int pos)
+{
+    Node *tmp = head;
+    for (int i = 1; i <= pos - 1; i++)
+    {
+        tmp = tmp->next;
+    }
+    Node *deleteNode = tmp->next;
+    tmp->next = tmp->next->next;
+    tmp->next->prev = tmp;
+    delete deleteNode;
+}
+
+void delete_tail(Node *&head, Node *&tail)
+{
+    Node *deleteNode = tail;
+    tail = tail->prev;
+    delete deleteNode;
+    if (tail == NULL)
+    {
+        head = NULL;
+        return;
+    }
+    tail->next = NULL;
+}
+
+void print_head_to(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+void print_tail_to(Node *tail)
+{
+    Node *tmp = tail;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->prev;
+    }
+    cout << endl;
+}
+
+int main()
+{ // Nodes
+    // Node *head = NULL;
+    // Node *tail = NULL;
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *tail = c;
+    // connections
+    head->next = a;
+    a->prev = head;
+
+    a->next = b;
+    b->prev = a;
+
+    b->next = c;
+    c->prev = b;
+
+    // print
+    int pos;
+    cin >> pos;
+
+    if (pos >= size(head))
+    {
+        cout << "Invalid" << endl;
+    }
+    else if (pos == 0)
+    {
+        delete_head(head, tail);
+    }
+    else if (pos == size(head) - 1)
+    {
+        delete_tail(head, tail);
+    }
+    else
+    {
+        delete_at_position(head, pos);
+    }
+
+    print_head_to(head);
+    print_tail_to(tail);
+
+    return 0;
+}
