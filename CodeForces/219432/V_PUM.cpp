607c546f7b8ce55cfc39e80c988944e4ba7fdd04
mmahmudul7 <hasanitgroup@gmail.com>
2024-07-03 23:05:24 +0600

    V PUM

---
+++ CodeForces/219432/V_PUM.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+    for (int i = 0; i < n * 4;)
+    {
+        cout << (i + 1) << " " << (i + 2) << " " << (i + 3) << " PUM" << endl;
+        i += 4;
+    }
+    return 0;
+}
