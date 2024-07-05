adabe33e9b9b2658e37383da6d22df9e60df5b8d
mmahmudul7 <hasanitgroup@gmail.com>
2024-07-05 23:58:15 +0600

    B. Drawing 'X'

---
+++ CodeForces/326907/B_Drawing_X.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+
+    for (int row = 0; row < n/2; row++){
+        for (int col = 0; col < n/2; col++){
+            if(row == col)
+                cout << "\\";
+            else
+                cout << "*";
+        }
+        for (int col = n/2 + 1; col < n; col++){
+            if(row + col == n - 1)
+                cout << "*/";
+            else
+                cout << "*";
+        }
+        cout << endl;        
+    }
+
+    for (int i = 0; i < n/2; i++)
+        cout << "*";
+    
+    cout << "X";
+    
+    for (int i = 0; i < n/2; i++)
+        cout << "*";
+    cout << endl;
+
+    for (int row = 0; row < n/2; row++){
+        for (int col = n/2 + 1; col < n; col++){
+            if(row + col == n - 1)
+                cout << "/";
+            else
+                cout << "*";
+        }
+        for (int col = 0; col < n/2; col++){
+            if(row == col)
+                cout << "*\\";
+            else
+                cout << "*";
+        }
+        cout << endl;        
+    }
+    
+    return 0;
+}
