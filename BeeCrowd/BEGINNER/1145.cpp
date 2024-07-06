d2d0a20405984a21250b779c2ad34f5f38f35b1f
mmahmudul7 <hasanitgroup@gmail.com>
2024-07-06 22:53:59 +0600

    Logical Sequence 2

---
+++ BeeCrowd/BEGINNER/1145.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x, y;
+    cin >> x >> y;
+
+    int n = 1;
+
+    for (int i = 1; i <= y; i++){
+        if (n == x){
+            cout << i << endl;
+            n = 1;
+        } else {
+            cout << i << " ";
+            n++;
+        }
+    }
+
+    return 0;
+}
