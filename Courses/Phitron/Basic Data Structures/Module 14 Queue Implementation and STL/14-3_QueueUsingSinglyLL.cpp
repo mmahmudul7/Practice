6e6ddf2e882fa955d0a8d5a10e73a9367bc14ae8
mmahmudul7 <hasanitgroup@gmail.com>
2024-07-08 13:48:45 +0600

    Queue Implement using Singly Linked List

---
+++ Courses/Phitron/Basic Data Structures/Module 14 Queue Implementation and STL/14-3_QueueUsingSinglyLL.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+        int val;
+        Node* next;
+
+        Node(int val){
+            this->val = val;
+            this->next = NULL;
+        }
+};
+
+class myQueue{
+    public:
+        Node *head = NULL;
+        Node *tail = NULL;
+        int sz = 0;
+        void push(int val){ // insert at tail
+            sz++; // sz++ opore rakhai safe zone, otherwise head null hole size er value increase hobe na
+            Node *newNode = new Node(val);
+            if(head == NULL){
+                head = newNode;
+                tail = newNode;
+                return;
+            }
+            tail->next = newNode;
+            tail = tail->next;
+        }
+        void pop(){ // delete from head
+            sz--;
+            Node *deleteNode = head;
+            head = head->next;
+            delete deleteNode;
+            if(head == NULL){
+                tail = NULL;
+            }
+        }
+        int front(){
+            return head->val;
+        }
+        int size(){
+            return sz;
+        }
+        bool empty(){
+            if(sz == 0)
+                return true;
+            else
+                return false;
+        }
+};
+
+int main(){
+    myQueue q;
+    int n;
+    cin >> n;
+    while (n--){
+        int x;
+        cin >> x;
+
+        q.push(x);
+    }
+    cout << endl;
+    while (!q.empty()){
+        cout << q.front() << " ";
+        q.pop();
+    }
+
+    /*
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    // q.pop();
+    // q.pop();
+    cout << q.front() << endl;
+    cout << q.size();
+    */
+    return 0;
+}
