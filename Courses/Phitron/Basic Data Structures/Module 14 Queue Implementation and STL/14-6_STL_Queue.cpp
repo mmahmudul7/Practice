6741105510e297e6b44a294843badf9bcb6724f1
mmahmudul7 <hasanitgroup@gmail.com>
2024-07-08 17:43:25 +0600

    STL Queue

---
+++ Courses/Phitron/Basic Data Structures/Module 14 Queue Implementation and STL/14-6_STL_Queue.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    queue<int> q;
+    int n;
+    cin >> n;
+    while (n--){
+        int x;
+        cin >> x;
+
+        q.push(x);
+    }
+    
+    cout << endl << q.size() <<endl;
+
+    while (!q.empty()){
+        cout << q.front() << " ";
+        q.pop();
+    }
+
+    return 0;
+}
