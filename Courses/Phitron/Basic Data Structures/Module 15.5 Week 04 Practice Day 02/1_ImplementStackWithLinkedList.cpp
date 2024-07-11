c29dc28552370329f7fc48356ab38a3a71d35dc0
mmahmudul7 <hasanitgroup@gmail.com>
2024-07-11 09:24:00 +0600

    Implement Stack With Linked List update

--- Courses/Phitron/Basic Data Structures/Module 15.5 Week 04 Practice Day 02/1_ImplementStackWithLinkedList.cpp
+++ Courses/Phitron/Basic Data Structures/Module 15.5 Week 04 Practice Day 02/1_ImplementStackWithLinkedList.cpp
@@ -26,6 +26,19 @@
 
 
 *****************************************************************/
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+
+        Node(int data){
+            this->data = data;
+            this->next = NULL;
+        }
+};
 
 class Stack {
 public:
