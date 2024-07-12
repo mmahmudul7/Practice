105b7052df693f13116dd7afc4df6bc89b472ec9
mmahmudul7 <hasanitgroup@gmail.com>
2024-07-12 17:16:19 +0600

    Hello, World!

---
+++ HackerRank/C++/cpp-hello-world.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    cout << "Hello, World!";
+    return 0;
+}
