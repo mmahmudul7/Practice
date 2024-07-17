5b2cf4cdad404808d7f0d5c42eaed37f81e932b5
mmahmudul7 <hasanitgroup@gmail.com>
2024-07-17 13:42:50 +0600

    vetor input sort print

---
+++ HackerRank/C++/STL/vector-sort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    vector<int> v;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    
+    sort(v.begin(), v.end()); 
+    
+    for(int val : v) {
+        cout << val << " ";
+    }
+      
+    return 0;
+}
