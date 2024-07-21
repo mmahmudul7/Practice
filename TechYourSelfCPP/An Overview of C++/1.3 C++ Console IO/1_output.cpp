1b097f3fefaf889e4d542e49d79dd4f9a0f6a079
mmahmudul7 <hasanitgroup@gmail.com>
2024-07-21 21:54:26 +0600

    Interget double value assign and output

---
+++ TechYourSelfCPP/An Overview of C++/1.3 C++ Console IO/1_output.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i, j;
+    double d;
+
+    i = 10;
+    j = 20;
+    d = 99.101;
+
+    cout << "Here are some values: ";
+    cout << i;
+    cout << ' ';
+    cout << j;
+    cout << ' ';
+    cout << d;
+    
+    return 0;
+}
