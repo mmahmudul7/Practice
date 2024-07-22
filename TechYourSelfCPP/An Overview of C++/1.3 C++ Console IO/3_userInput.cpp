0acc202982e028aaa6e174d6af0360c0efd9f949
mmahmudul7 <hasanitgroup@gmail.com>
2024-07-22 22:00:34 +0600

    Cin IO Expression

---
+++ TechYourSelfCPP/An Overview of C++/1.3 C++ Console IO/3_userInput.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i;
+
+    cout << "Enter a value: ";
+    cin >> i;
+    cout << "Here's your number: " << i << "\n";
+
+    return 0;
+}
