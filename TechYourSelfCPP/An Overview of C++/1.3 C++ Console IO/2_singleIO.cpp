93ca418703de06d78a0d158f1c8f833ac8b33401
mmahmudul7 <hasanitgroup@gmail.com>
2024-07-22 21:55:57 +0600

    Print multiple values using single io experession

---
+++ TechYourSelfCPP/An Overview of C++/1.3 C++ Console IO/2_singleIO.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i, j;
+    double d;
+
+    i = 10;
+    j = 20;
+    d = 99.101;
+
+    cout << "Here are some values: ";
+    cout << i << ' ' << j << ' ' << d;
+    
+    return 0;
+}
