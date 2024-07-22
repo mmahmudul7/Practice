35d7f36767c5eb7ad1d7d7adbdbe3e2f4abe8955
mmahmudul7 <hasanitgroup@gmail.com>
2024-07-22 22:27:00 +0600

    Amout of Pay

---
+++ TechYourSelfCPP/An Overview of C++/1.3 C++ Console IO/Exercises/1_salary.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int hours, wages;
+    cout << "Please enter working hours and wages: ";
+    cin >> hours >> wages;
+    int pay = hours * wages;
+
+    cout << pay;
+    
+    return 0;
+}
