03d28dc6010361daa7ebbb681e907b88396c6c99
mmahmudul7 <hasanitgroup@gmail.com>
2024-07-22 22:36:15 +0600

    Feet to Inches

---
+++ TechYourSelfCPP/An Overview of C++/1.3 C++ Console IO/Exercises/2_feetToInches.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float feet, inches;
+    cout << "Zero keys, 0 to stop. \n";
+    do {
+        cout << "Please enter the feet amout: ";
+        cin >> feet;
+
+        inches = feet * 12;
+
+        cout << feet << " = " << inches << " inches." <<"\n";
+    } while (feet != 0);
+    
+    return 0;
+}
