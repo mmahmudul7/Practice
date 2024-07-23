01a17756a2c08eb6c50ea94a4436cab4d4c40402
mmahmudul7 <hasanitgroup@gmail.com>
2024-07-23 16:18:34 +0600

    Nested comments

---
+++ TechYourSelfCPP/An Overview of C++/1.4 C++ Comments/2_nestedComments.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    // This is a strange /* way to do a comment */
+    // Makes it easier ofr you to "comment out" serveral lines
+
+    return 0;
+}
