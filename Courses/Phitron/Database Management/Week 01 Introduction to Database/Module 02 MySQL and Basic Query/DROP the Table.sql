a1472b2f5e12bb5feff4e608d3998929baa55cb8
mmahmudul7 <hasanitgroup@gmail.com>
2024-10-28 16:40:59 +0600

    Create Database table drop

---
+++ Courses/Phitron/Database Management/Week 01 Introduction to Database/Module 02 MySQL and Basic Query/DROP the Table.sql
@@ -0,0 +1,9 @@
+USE ProgrammingHero;
+
+CREATE TABLE Student (
+	Roll CHAR(4),
+    Name VARCHAR(50),
+    Marks DOUBLE
+);
+
+DROP TABLE Student;
---
+++ Courses/Phitron/Database Management/Week 01 Introduction to Database/Module 02 MySQL and Basic Query/createTableWithPrimaryKey.sql
@@ -0,0 +1,7 @@
+USE ProgrammingHero;
+
+CREATE TABLE Student (
+	Roll CHAR(4) PRIMARY KEY,
+    Name VARCHAR(50),
+    Marks DOUBLE
+);
