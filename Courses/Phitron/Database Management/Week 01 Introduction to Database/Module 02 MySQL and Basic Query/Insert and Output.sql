1fa32b735abff3a8f457b895ef03e52f030183ac
mmahmudul7 <hasanitgroup@gmail.com>
2024-10-28 18:07:34 +0600

    Insert and Show the table

---
+++ Courses/Phitron/Database Management/Week 01 Introduction to Database/Module 02 MySQL and Basic Query/Insert and Output.sql
@@ -0,0 +1,12 @@
+USE ProgrammingHero;
+
+INSERT INTO Student
+(Roll, Name, Marks) VALUES(1, 'Hasan', 95);
+
+INSERT INTO Student
+(Roll, Name, Marks) VALUES(2, 'Mahmud', 99);
+
+INSERT INTO Student
+(Roll, Name) VALUES(3, 'Shamim');
+
+SELECT * FROM ProgrammingHero.Student;
