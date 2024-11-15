e41a48119237cbf95fe41f0424fbf92bede838f4
mmahmudul7 <hasanitgroup@gmail.com>
2024-11-15 12:24:10 +0600

    Comments Arithmetic Comparison

---
+++ Courses/Python Guideline Series/forth.py
@@ -0,0 +1,44 @@
+# Single line comment
+
+'''
+Multiline comment
+'''
+# ctrl + /
+
+# Python Arithmetic Operators
+# + Addition
+# - Subtraction
+# * Multiplication
+# / Division
+# % Modulus
+# ** Exponentiation
+# // Floor division
+print("Arithmetic Operators")
+a = 15
+b = 2
+print(a + b) # 17
+print(a - b) # 13
+print(a * b) # 30
+print(a / b) # 7.5
+print(a % b) # 1
+print(2 ** 3) # 2 ^ 3 = 8
+print(a // b) # 7
+print()
+'''
+Python Comparison Operators:
+== Equal
+!= Not equal
+> Greater than
+< Less than
+>= Greater than or equal to
+<= Less than or equal to
+'''
+
+# boolean value retrun True or False
+print("Comparison")
+print(5 == 3)
+print(5 != 3)
+print(5 > 3)
+print(5 < 3)
+print(5 >= 3)
+print(5 <= 3)
