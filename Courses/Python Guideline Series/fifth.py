69ebbc4d4d725032ebc82decf01bae34111a0fb8
mmahmudul7 <hasanitgroup@gmail.com>
2024-11-15 12:23:34 +0600

    If Else

---
+++ Courses/Python Guideline Series/fifth.py
@@ -0,0 +1,13 @@
+# problem 1
+rain = True
+if rain == True:
+    print("Ami shool e jabo na")
+else:
+    print("Ami shool e jabo")
+
+# problem 2
+age = 17
+if age >= 18:
+    print("Tumi license paba")
+else:
+    print("Tumi 18-")
