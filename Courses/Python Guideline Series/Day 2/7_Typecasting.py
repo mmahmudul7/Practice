ddb77ac8fdc28d43ca5aa9b8445365be8688d59e
mmahmudul7 <hasanitgroup@gmail.com>
2024-11-17 18:18:17 +0600

    renamed

---
+++ Courses/Python Guideline Series/Day 2/7_Typecasting.py
@@ -0,0 +1,22 @@
+'''
+age = int(input())
+
+if age >= 18:
+    print("you are audlt")
+else:
+    print("you are child")
+'''
+
+weight = float(input())
+print()
+
+print(weight)
+print(type(weight))
+
+print(int(weight))
+new_weight = int(weight)
+new_new_weight = float(int(weight))
+print(new_new_weight)
+print(type(new_new_weight))
+
+
