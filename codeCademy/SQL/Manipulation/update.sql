3a1534fe0597fb8ef50d2dcdc4787ac56dfcbbd5
mmahmudul7 <hasanitgroup@gmail.com>
2024-11-22 17:36:51 +0600

    update

---
+++ codeCademy/SQL/Manipulation/update.sql
@@ -0,0 +1,9 @@
+-- The UPDATE statement edits a row in a table.
+
+UPDATE celebs 
+SET twitter_handle = '@mmahmudul7' 
+WHERE id = 4; 
+
+SELECT * FROM celebs;
+
+-- SET is a clause that indicates the column to edit.
