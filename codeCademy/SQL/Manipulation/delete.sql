8565bc4626cf93876bcf7986913414d044dbb1d0
mmahmudul7 <hasanitgroup@gmail.com>
2024-11-22 17:35:07 +0600

    delete

---
+++ codeCademy/SQL/Manipulation/delete.sql
@@ -0,0 +1,6 @@
+-- The statement below deletes all records 
+-- DELETE FROM is a clause that lets you delete rows from a table with WHERE Cluse
+
+DELETE FROM celebs 
+WHERE twitter_handle IS NULL;
+
