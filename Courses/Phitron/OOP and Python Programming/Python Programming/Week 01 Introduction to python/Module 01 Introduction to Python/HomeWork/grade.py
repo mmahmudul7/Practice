53d0dbdecbdfd0d328d6e7cc6c758fcbbf8840bc
mmahmudul7 <hasanitgroup@gmail.com>
2024-11-24 21:11:59 +0600

    Grade

---
+++ Courses/Phitron/OOP and Python Programming/Python Programming/Week 01 Introduction to python/Module 01 Introduction to Python/HomeWork/grade.py
@@ -0,0 +1,16 @@
+num = 95
+
+if num <= 100 and num >= 80:
+    print("A+")
+elif num < 80 and num >= 70:
+    print("A")
+elif num < 70 and num >= 60:
+    print("A-")
+elif num < 60 and num >= 50:
+    print("B")
+elif num < 50 and num >= 40:
+    print("C")
+elif num < 40 and num >= 33:
+    print("D")
+else:
+    print("F")
