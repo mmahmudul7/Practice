a52ccbd12d7f15b4d211d1e225b72f2169990146
mmahmudul7 <hasanitgroup@gmail.com>
2024-11-26 00:02:59 +0600

    Buil In

---
+++ Courses/Phitron/OOP and Python Programming/Python Programming/Week 01 Introduction to python/Module 02 Basic Python/2-5_built-inFunctions.py
@@ -0,0 +1,9 @@
+# https://docs.python.org/3/library/functions.html
+
+# highest = max(45, 98, 46, 8, 96, 41)
+highest = max([45, 98, 46, 8, 96, 41])
+smallest = min([45, 98, 46, 8, 96, 41])
+count = len([45, 98, 46, 8, 96, 41])
+total = sum([45, 98, 46, 8, 96, 41])
+
+print(highest, smallest, count, total)
