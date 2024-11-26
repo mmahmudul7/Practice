7afaff1dd40e52b5317dda393e90e64b3b7bbf3c
mmahmudul7 <hasanitgroup@gmail.com>
2024-11-26 20:43:20 +0600

    break

---
+++ Courses/Python Guideline Series/Day 4/18_break.py
@@ -0,0 +1,21 @@
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
+
+print()
+
+j = 0
+while j <= 10:
+    print(j)
+    j += 1
+    if j == 5:
+        break
+
+print()
+a = 1
+while a <= 10:
+    a += 1
+    print(a)
+    if a == 5:
+        break
