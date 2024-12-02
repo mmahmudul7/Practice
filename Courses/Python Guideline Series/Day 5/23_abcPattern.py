d3444c2a05506df31a074d7e599793f8ed85afbf
mmahmudul7 <hasanitgroup@gmail.com>
2024-12-02 19:53:20 +0600

    abc pattern

---
+++ Courses/Python Guideline Series/Day 5/23_abcPattern.py
@@ -0,0 +1,11 @@
+for row in range(5):
+    for col in range(row + 1):
+        print(chr(65 + row), end = " ")
+    print()
+
+print()
+
+for row in range(5):
+    for col in range(row + 1):
+        print(chr(97 + row), end = " ")
+    print()
