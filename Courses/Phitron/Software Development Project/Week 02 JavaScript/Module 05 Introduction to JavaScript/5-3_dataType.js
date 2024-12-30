dcde322ab319f9466bea07d5c3fba75a4c46b69d
mmahmudul7 <hasanitgroup@gmail.com>
2024-12-30 19:00:14 +0600

    Data Type

--- Courses/Phitron/Software Development Project/Week 02 JavaScript/Module 05 Introduction to JavaScript/5-3_dataType.js
+++ Courses/Phitron/Software Development Project/Week 02 JavaScript/Module 05 Introduction to JavaScript/5-3_dataType.js
@@ -8,9 +8,14 @@ var obj = {
     model: "16 pro max",
     release: 2024
 }
+var hobbies = ["coding", "reading"];
+var fun = function() {};
+
 
 console.log(typeof myMsg);
 console.log(typeof intNum);
 console.log(typeof floatNum);
 console.log(typeof real);
 console.log(typeof obj);
+console.log(typeof hobbies); // object (Array and object)
+console.log(typeof fun);
