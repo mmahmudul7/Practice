fb0a13df707f6b205964594489bc7815cbf9e6f9
mmahmudul7 <hasanitgroup@gmail.com>
2024-12-30 13:10:27 +0600

    Data type

---
+++ Courses/Phitron/Software Development Project/Week 02 JavaScript/Module 05 Introduction to JavaScript/5-3_dataType.js
@@ -0,0 +1,16 @@
+var myMsg = "Hello World!";
+var intNum = 2;
+var floatNum = 25.02;
+var real = true;
+var obj = {
+    name: "Phone",
+    brand: "iPhone",
+    model: "16 pro max",
+    release: 2024
+}
+
+console.log(typeof myMsg);
+console.log(typeof intNum);
+console.log(typeof floatNum);
+console.log(typeof real);
+console.log(typeof obj);
