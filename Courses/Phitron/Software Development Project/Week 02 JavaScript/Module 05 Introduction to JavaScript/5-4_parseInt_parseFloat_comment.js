e21c64090a90de8cb2be3fa9d1e6a7cd78704dab
mmahmudul7 <hasanitgroup@gmail.com>
2024-12-30 19:42:46 +0600

    parseInt parseFloat Comments

--- Courses/Phitron/Software Development Project/Week 02 JavaScript/Module 05 Introduction to JavaScript/5-4_parseInt_parseFloat_comment.js
+++ Courses/Phitron/Software Development Project/Week 02 JavaScript/Module 05 Introduction to JavaScript/5-4_parseInt_parseFloat_comment.js
@@ -14,3 +14,8 @@ var num2 = parseFloat(num)
 var sum = num1 + num2
 
 console.log(sum)
+
+console.log(parseInt("101", 2)); // In Binary calculation
+
+console.log(parseInt("Hello"));    // Output: NaN
+console.log(parseFloat("Hello")); // Output: NaN
