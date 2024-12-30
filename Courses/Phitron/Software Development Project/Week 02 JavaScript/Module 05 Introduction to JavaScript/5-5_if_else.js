a1303132d6d1d6ea64b91d56ce9d599e9296b457
mmahmudul7 <hasanitgroup@gmail.com>
2024-12-30 19:48:03 +0600

    if else

--- Courses/Phitron/Software Development Project/Week 02 JavaScript/Module 05 Introduction to JavaScript/5-5_if_else.js
+++ Courses/Phitron/Software Development Project/Week 02 JavaScript/Module 05 Introduction to JavaScript/5-5_if_else.js
@@ -4,7 +4,7 @@ if (marks >= 90 && marks <= 100) {
     console.log("Grade: A+");
 } else if (marks >= 80) {
     console.log("Grade: A");
-} else if (marks >= 70 || marks >= 60) {
+} else if (marks >= 70 || marks <= 60) {
     console.log("Grade: B");
 } else {
     console.log("Grade: C");
