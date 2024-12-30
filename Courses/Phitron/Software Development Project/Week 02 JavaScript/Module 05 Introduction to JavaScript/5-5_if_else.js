9074a54914399ca53eaa3eb1fe245e025c1ba10a
mmahmudul7 <hasanitgroup@gmail.com>
2024-12-30 19:46:19 +0600

    if else

---
+++ Courses/Phitron/Software Development Project/Week 02 JavaScript/Module 05 Introduction to JavaScript/5-5_if_else.js
@@ -0,0 +1,16 @@
+let marks = 85;
+
+if (marks >= 90 && marks <= 100) {
+    console.log("Grade: A+");
+} else if (marks >= 80) {
+    console.log("Grade: A");
+} else if (marks >= 70 || marks >= 60) {
+    console.log("Grade: B");
+} else {
+    console.log("Grade: C");
+}
+
+// Ternary Operator
+let age = 18;
+let status = (age >= 18) ? "Adult" : "Not Adult";
+console.log(status);
