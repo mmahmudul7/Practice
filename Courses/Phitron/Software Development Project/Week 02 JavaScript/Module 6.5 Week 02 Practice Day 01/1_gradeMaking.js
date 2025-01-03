dc146d2fd3e6d082466fc9e41fe97fa1ca0622d7
mmahmudul7 <hasanitgroup@gmail.com>
2025-01-03 23:33:36 +0600

    If else Grade

---
+++ Courses/Phitron/Software Development Project/Week 02 JavaScript/Module 6.5 Week 02 Practice Day 01/1_gradeMaking.js
@@ -0,0 +1,11 @@
+let marks = 85;
+
+if (marks >= 90 && marks <= 100) {
+    console.log("Grade: A+");
+} else if (marks >= 80) {
+    console.log("Grade: A");
+} else if (marks >= 70 || marks <= 60) {
+    console.log("Grade: B");
+} else {
+    console.log("Grade: C");
+}
