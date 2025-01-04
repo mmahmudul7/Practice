4421e0f097ead639a2546acc6c6e4e0e8ae050cb
mmahmudul7 <hasanitgroup@gmail.com>
2025-01-04 19:10:52 +0600

    Add Event Listener

---
+++ Courses/Phitron/Software Development Project/Week 02 JavaScript/Module 07 API & DOM with JavaScript-boost/7-4_addEventListener.js
@@ -0,0 +1,12 @@
+document.getElementById("handleADD").addEventListener("click", (event) => {
+    const inputValue = document.getElementById("search-box").value;
+    console.log(inputValue);
+});
+
+// document.getElementById("handleADD").addEventListener("click", (event) => {
+//     console.log("Hello World!");
+// });
+
+// const handleSearch = (event) => {
+//     console.log("Hello World!");
+// };
