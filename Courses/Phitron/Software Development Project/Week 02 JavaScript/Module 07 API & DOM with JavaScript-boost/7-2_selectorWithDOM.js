6a746e983cbb26409e1cf8b8652f26b540e0b1a4
mmahmudul7 <hasanitgroup@gmail.com>
2025-01-04 11:28:54 +0600

    Selector with DOM

---
+++ Courses/Phitron/Software Development Project/Week 02 JavaScript/Module 07 API & DOM with JavaScript-boost/7-2_selectorWithDOM.js
@@ -0,0 +1,8 @@
+// const target = document.getElementsByClassName("title");
+// console.log(target);
+// console.log(target[0]);
+
+const target = document.getElementById("title");
+
+// target.style.color = "red";
+target.style.color = "#0000ff";
