fc7f66587d3fdc0b5f602948bf75fdc63318cf4d
mmahmudul7 <hasanitgroup@gmail.com>
2025-01-07 19:44:48 +0600

    update

--- Courses/Python Guideline Series/Day 3 Practice Problems Day 1 and Day 2/1_squreOrNot.py
+++
@@ -1,9 +0,0 @@
-# Take values of length and breadth of a rectangle from user and check if it is square or not
-
-length = int(input("Enter the length of rectangle: "))
-breadth = int(input("Enter the breadth of rectangle: "))
-
-if length == breadth:
-    print("Square")
-else:
-    print("Not Square")
