3e0b6eda3e2076c91f5fda4db7b278bc73d6ac81
mmahmudul7 <hasanitgroup@gmail.com>
2025-01-10 19:32:08 +0600

    List Slicing

---
+++ Courses/Python Guideline Series/Day 8/33_ListSlicing.py
@@ -0,0 +1,9 @@
+list = [12, 13, 4, "phrtron", "Mango", [12.5, 19]]
+
+print(list)
+print(list[::])
+print(list[1::])
+print(list[2::4])
+print(list[::-1]) # reverse
+print(list[-1:-3:-1])
+print(list[-1:-1:-1]) # []
