a5d72fc9db6b02920ed174b26e8b81c85f6f6497
mmahmudul7 <hasanitgroup@gmail.com>
2025-10-12 00:49:21 +0600

    revision

--- Courses/Phitron/OOP and Python Programming/Python Programming/Week 01 Introduction to python/Module 02 Basic Python/2-7_lsit_method.py
+++ Courses/Phitron/OOP and Python Programming/Python Programming/Week 01 Introduction to python/Module 02 Basic Python/2-7_lsit_method.py
@@ -1,31 +1,31 @@
 numbers = [12, 45, 98, 68]
-print(numbers)
+print(numbers) # [12, 45, 98, 68]
 
 numbers.append(56)
-print(numbers)
+print(numbers) # [12, 45, 98, 68, 56] 
 
 numbers.insert(2, 81) # insert in 2nd index
-print(numbers)
+print(numbers) # [12, 45, 81, 98, 68, 56]
 
 if 98 in numbers:
     numbers.remove(98)
-print(numbers)
+print(numbers) # [12, 45, 81, 68, 56]
 
 if 8 in numbers:
     numbers.remove(8)
-print(numbers)
+print(numbers) # [12, 45, 81, 68, 56]
 
 lastElement = numbers.pop()
-print(lastElement, numbers)
+print(lastElement, numbers) # 56 [12, 45, 81, 68]
 
 if 45 in numbers:
     index = numbers.index(45)
-    print(index)
+    print(index) # 1
 
 numbers.sort() 
-print(numbers)
+print(numbers) # [12, 45, 68, 81]
 
 numbers.reverse()
-print(numbers)
+print(numbers) # [81, 68, 45, 12]
 
 # etc
