a8509a3ab4c85f17be05bc4be329d977b74c33ac
mmahmudul7 <hasanitgroup@gmail.com>
2025-10-28 20:22:28 +0600

    random module

---
+++ pythonCrashCourse/Chapter_9_Classes/5_random_choice.py
@@ -0,0 +1,6 @@
+from random import choice
+
+# players = ['charles', 'martina', 'michael', 'florence', 'eli']
+players = ('charles', 'martina', 'michael', 'florence', 'eli')
+first_up = choice(players) #  This function takes in a list or tuple
+print(first_up)
