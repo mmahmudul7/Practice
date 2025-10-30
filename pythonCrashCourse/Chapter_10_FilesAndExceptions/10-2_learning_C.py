3eafd8d1dce8a52bcabea1dab804092550acf941
mmahmudul7 <hasanitgroup@gmail.com>
2025-10-30 20:01:23 +0600

    Replace Python by C

---
+++ pythonCrashCourse/Chapter_10_FilesAndExceptions/10-2_learning_C.py
@@ -0,0 +1,26 @@
+"""10-2. 
+Learning C: You can use the replace() method to replace any word in a string
+with a different word. Here’s a quick example showing how to replace 'dog' with 'cat'
+in a sentence:
+>>> message = "I really like dogs."
+>>> message.replace('dog', 'cat')
+'I really like cats.'
+Read in each line from the file you just created, learning_python.txt, and replace the
+word Python with the name of another language, such as C. Print each modified line to
+the screen.
+"""
+from pathlib import Path
+
+path = Path('learning_python.txt')
+contents_python = path.read_text()
+print(contents_python)
+
+print()
+contents_c = contents_python.replace('Python', 'C')
+print(contents_c)
+
+print()
+lines = contents_python.splitlines()
+for line in lines:
+    line = line.replace('Python', 'C')
+    print(line)
