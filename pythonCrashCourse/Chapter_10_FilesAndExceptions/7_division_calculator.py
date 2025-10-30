8102c014ebfb26e27c826c499ea7843343aa33b8
mmahmudul7 <hasanitgroup@gmail.com>
2025-10-31 00:59:03 +0600

    try-except ZeroDivisionError The else Block

---
+++ pythonCrashCourse/Chapter_10_FilesAndExceptions/7_division_calculator.py
@@ -0,0 +1,19 @@
+# Using Exception to Prevent Crashes 
+print("Give me two numbers, and I'll divide them.")
+print("Enter 'q' to quit.")
+
+while True:
+    first_number = input("\nFirst number: ")
+    if first_number == 'q':
+        break
+    second_number = input("Second number: ")
+    if second_number == 'q':
+        break
+    try:
+        answer = int(first_number) / int(second_number)
+    except ZeroDivisionError:
+        print("You can't divide by 0!")
+    else:
+        print(answer)
+
+# If the try block was successful, this code goes in the else block. 
