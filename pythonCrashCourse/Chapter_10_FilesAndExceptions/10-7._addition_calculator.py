9d868c35e1bb2aa445a12bfeb2d322cbeab42b08
mmahmudul7 <hasanitgroup@gmail.com>
2025-10-31 23:28:52 +0600

    ValueError Additional Calculator

---
+++ pythonCrashCourse/Chapter_10_FilesAndExceptions/10-7._addition_calculator.py
@@ -0,0 +1,24 @@
+"""10-7. 
+Addition Calculator: Wrap your code from Exercise 10-5 in a while loop so the
+user can continue entering numbers, even if they make a mistake and enter text
+instead of a number.
+"""
+print("Enter 'q' at any time to quit.\n")
+
+while True:
+    try:
+        number_one = input("Please inter 1st number: ")
+        if number_one == 'q':
+            break
+        number_one = int(number_one)
+
+        number_two = int(input("Please inter 2nd number: "))
+        if number_two == 'q':
+            break
+        number_two = int(number_two)
+
+    except ValueError:
+        print("Sorry, I really need a number.")
+    else:
+        sum = number_one + number_two
+        print(f"The sum of {number_one} and {number_two} is {sum}.")
