96e97251b40b3dfeb50f5d549b7a86d556316646
mmahmudul7 <hasanitgroup@gmail.com>
2025-11-01 02:01:31 +0600

    Reading Data Using json.loads()

---
+++ pythonCrashCourse/Chapter_10_FilesAndExceptions/11_number_reader.py
@@ -0,0 +1,11 @@
+# Reading Data Using json.loads()
+from pathlib import Path
+import json
+
+path = Path('numbers.json')
+contents = path.read_text()
+numbers = json.loads(contents)
+
+print(numbers)
+
+# This is sample way to share data between two programs.
