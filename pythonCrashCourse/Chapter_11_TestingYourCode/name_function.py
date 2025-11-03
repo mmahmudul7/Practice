fdd89b2aab7d76cf98faf587d563b47ae7f319b8
mmahmudul7 <hasanitgroup@gmail.com>
2025-11-04 00:10:50 +0600

    A Failing Test

--- pythonCrashCourse/Chapter_11_TestingYourCode/name_function.py
+++ pythonCrashCourse/Chapter_11_TestingYourCode/name_function.py
@@ -1,5 +1,5 @@
-# Testing a Function 
-def get_formatted_name(first, last):
+# A Failing Test
+def get_formatted_name(first, middle, last):
     """Generate a neatly formatted full name."""
-    full_name = f"{first} {last}"
+    full_name = f"{first} {middle} {last}"
     return full_name.title()
