fb99a6171e2e84eebc1f6c25d20884fa712e3fdd
mmahmudul7 <hasanitgroup@gmail.com>
2025-11-12 23:53:57 +0600

    ex-4 Rocket moving up right down left

---
+++ pythonCrashCourse/Chapter_12_aShipThatFiresBullets/ex_4_rocket/rocket.py
@@ -0,0 +1,34 @@
+import pygame
+
+class Rocket:
+
+    def __init__(self, r_game):
+        self.screen = r_game.screen
+        self.settings = r_game.settings
+        self.screen_rect = r_game.screen.get_rect()
+        self.image = pygame.image.load('images/rocket_small.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = self.screen_rect.center
+
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+        self.moving_right, self.moving_left = False, False
+        self.moving_up, self.moving_down = False, False
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.rocket_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.rocket_speed    
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.rocket_speed
+        if self.moving_down and self.rect.bottom <= self.screen_rect.bottom:
+            self.y += self.settings.rocket_speed
+
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
---
+++ pythonCrashCourse/Chapter_12_aShipThatFiresBullets/ex_4_rocket/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+
+    def __init__(self):
+        self.screen_width = 1280
+        self.screen_height = 720
+        self.bg_color = (255, 255, 255)
+
+        self.rocket_speed = 1.5
