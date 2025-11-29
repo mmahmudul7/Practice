b19e90c348a9085219db02ea30e14caf1b9a02ee
mmahmudul7 <hasanitgroup@gmail.com>
2025-11-29 23:56:13 +0600

    Writing Rain drop games

---
+++ pythonCrashCourse/Chapter_13_Aliens/Exercises/ex_3_rain_drops/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+
+    def __init__(self):
+        self.screen_width = 1365
+        self.screen_height = 765
+        self.bg_color = (230, 230, 230)
+
+        self.raindrop_speed = 1.5
