d2d79179a967b0706e89bdbea54c475c44232197
mmahmudul7 <hasanitgroup@gmail.com>
2025-11-30 01:02:14 +0600

    Rain drops games

--- pythonCrashCourse/Chapter_13_Aliens/Exercises/ex_3_rain_drops/raindrops_game.py
+++ pythonCrashCourse/Chapter_13_Aliens/Exercises/ex_3_rain_drops/raindrops_game.py
@@ -4,33 +4,50 @@ import pygame
 from settings import Settings
 from raindrop import Raindrop
 
-class RaindropgGame:
+class RaindropsGame:
     
     def __init__(self):
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.settings = Settings
+        self.settings = Settings()
 
         self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height)
-        )
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Raindrops")
 
         self.raindrops = pygame.sprite.Group()
         self._create_drops()
 
+    def run_game(self):
+        while True:
+            self._check_events()
+            self.raindrops.update()
+            self._update_screen()
+            self.clock.tick(60)
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_q:
+            sys.exit()
+
     def _create_drops(self):
         drop = Raindrop(self)
         drop_width, drop_height = drop.rect.size
-        
+
         current_x, current_y = drop_width, drop_height
         while current_y < (self.settings.screen_height - 2 * drop_height):
             while current_x < (self.settings.screen_width - 2 * drop_width):
                 self._create_drop(current_x, current_y)
                 current_x += 2 * drop_width
 
-                current_x = drop_width
-                current_y += 2 * drop_height
+            current_x = drop_width
+            current_y += 2 * drop_height
 
     def _create_drop(self, x_position, y_position):
         new_drop = Raindrop(self)
@@ -38,3 +55,14 @@ class RaindropgGame:
         new_drop.rect.x = x_position
         new_drop.rect.y = y_position
         self.raindrops.add(new_drop)
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.raindrops.draw(self.screen)
+
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    rd_game = RaindropsGame()
+    rd_game.run_game()
