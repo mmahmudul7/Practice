22ff93d816b333c47adb6ad215e82c4755efcd9f
mmahmudul7 <hasanitgroup@gmail.com>
2025-12-12 20:55:54 +0600

    define function, parameters, arguments and pass by value

---
+++ Automate The Boring Stuff with Phython/Chapter_3_Functions/2_hello_fun_2.py
@@ -0,0 +1,9 @@
+# hello custom function, define by def keyword
+# parameter is forgotten stored value, when the function returns
+def hello(name): # name called parameter, which is store argument
+    print(f'Hello {name}')
+
+
+hello('Alice') # 'Alice' is argument
+hello('Bop') # calling hello() function
+hello('Mahmud') # passing value 'Mahmud' to function hello()
