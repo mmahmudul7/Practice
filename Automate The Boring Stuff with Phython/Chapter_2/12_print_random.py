5e480fed21ebe2baef5d741282dacdc2d4fb7b70
mmahmudul7 <hasanitgroup@gmail.com>
2025-12-12 18:55:59 +0600

    Importing Modules random and sys

---
+++ Automate The Boring Stuff with Phython/Chapter_2/12_print_random.py
@@ -0,0 +1,11 @@
+import random
+
+for i in range(5):
+    print(random.randint(1, 10))
+
+# print(random.randint(1, 10))
+
+# from random import randint
+
+# for i in range(5):
+#     print(randint(1, 10))
