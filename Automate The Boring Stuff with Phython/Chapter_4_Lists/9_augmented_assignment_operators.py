0f419c5f07c2c965fc25063c50e9c4b573b2947b
mmahmudul7 <hasanitgroup@gmail.com>
2025-12-15 00:26:43 +0600

    Augmented Assignment Operators

---
+++ Automate The Boring Stuff with Phython/Chapter_4_Lists/9_augmented_assignment_operators.py
@@ -0,0 +1,21 @@
+spam = 42
+spam = spam + 1 # assignment statement
+print(spam)
+
+spam = 42
+spam += 1 # augmented assignment statement
+# spam -= 1
+# spam *= 1
+# spam /= 1
+# spam %= 1
+print(spam)
+
+# String concatenation 
+spam = 'Hello'
+spam += ' world!'
+print(spam)
+
+# String list replication 
+spam = ['Zophie']
+spam *= 3
+print(spam)
