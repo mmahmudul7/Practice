1ea50a4c36b6b35bace5e1a81f969a7efce07dcd
mmahmudul7 <hasanitgroup@gmail.com>
2025-12-14 12:08:35 +0600

    tuple unpaking

---
+++ Automate The Boring Stuff with Phython/Chapter_4_Lists/5_tuple_unpaking.py
@@ -0,0 +1,9 @@
+# cat = ['fat', 'gray', 'loud']
+# size = cat[0]
+# color = cat[1]
+# disposition = cat[2]
+
+cat = ['fat', 'gray', 'loud']
+size, color, disposition = cat
+
+print(f"Size: {size}, Color: {color}, Disposition: {disposition}")
