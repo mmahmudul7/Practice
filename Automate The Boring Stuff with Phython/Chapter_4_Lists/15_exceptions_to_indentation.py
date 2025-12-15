7c67441bc58e37ef2db77397b46da515d92f9302
mmahmudul7 <hasanitgroup@gmail.com>
2025-12-15 13:32:53 +0600

    Indentations

---
+++ Automate The Boring Stuff with Phython/Chapter_4_Lists/15_exceptions_to_indentation.py
@@ -0,0 +1,23 @@
+spam = ['apples',
+'oranges',
+            'bananas',
+'cats']
+
+print(spam)
+
+
+print('Four score and seven ' + \
+'years ago...')
+
+print('''
+apples
+      organge
+                bananas
+                        cats
+''')
+
+print("""
+      Four score and seven
+      
+years ago...
+""")
