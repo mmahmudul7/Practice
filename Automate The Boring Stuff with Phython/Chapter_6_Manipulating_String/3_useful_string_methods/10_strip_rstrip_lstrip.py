840d469a517c10fbef849ca02190c46fd3c71951
mmahmudul7 <hasanitgroup@gmail.com>
2025-12-20 03:43:49 +0600

    strip, lstrip and rstrip methods

---
+++ Automate The Boring Stuff with Phython/Chapter_6_Manipulating_String/3_useful_string_methods/10_strip_rstrip_lstrip.py
@@ -0,0 +1,12 @@
+spam = '    Hello, World!    '
+
+print(spam)
+
+print(spam.strip())
+print(spam.lstrip())
+print(spam.rstrip())
+
+# Passing argument 
+spam = 'SpamSpamBaconSpamEggsSpamSpam'
+print(spam)
+print(spam.strip('ampS'))
