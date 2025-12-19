ba03165ccdfd4cf97f12911631d4736b7e1da509
mmahmudul7 <hasanitgroup@gmail.com>
2025-12-20 04:16:26 +0600

    ord, chr, copy and paste functions

---
+++ Automate The Boring Stuff with Phython/Chapter_6_Manipulating_String/4_functions/1_ord_and_chr_functions.py
@@ -0,0 +1,18 @@
+# Text to Unicode code point 
+print(ord('A'))
+print(ord('4'))
+print(ord('!'))
+
+# Unicode code point to Text
+print()
+print(chr(65))
+print(chr(52))
+print(chr(33))
+
+# ========
+print()
+print(ord('B'))
+print(ord('A') < ord('B'))
+
+print(chr(ord('A')))
+print(chr(ord('A') + 1))
