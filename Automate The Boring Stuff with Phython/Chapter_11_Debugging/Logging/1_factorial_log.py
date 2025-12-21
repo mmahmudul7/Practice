fe9dd1acc1fa420cf10af01000eb434364ce7a23
mmahmudul7 <hasanitgroup@gmail.com>
2025-12-21 14:15:12 +0600

    change range(n + 1) to range(1, n + 1)

--- Automate The Boring Stuff with Phython/Chapter_11_Debugging/Logging/1_factorial_log.py
+++ Automate The Boring Stuff with Phython/Chapter_11_Debugging/Logging/1_factorial_log.py
@@ -12,7 +12,7 @@ def factorial(n):
     logging.debug('Start of factorial(%s)', n)
     total = 1
 
-    for i in range(n + 1):
+    for i in range(1, n + 1):
         total *= i
         logging.debug('i is %s, total is %s', i, total)
 
