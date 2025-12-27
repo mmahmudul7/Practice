cdfee4433508d0a8ea5e9fe9378c89ed4ccf5500
mmahmudul7 <hasanitgroup@gmail.com>
2025-12-27 13:57:48 +0600

    end_other solve without endswith()

--- Problem_Solving/Beginner/7_String_2/5_end_other.py
+++ Problem_Solving/Beginner/7_String_2/5_end_other.py
@@ -3,3 +3,25 @@ def end_other(a, b):
   s2 = b.lower()
   
   return s1.endswith(s2) or s2.endswith(s1)
+
+
+##
+def end_other(a, b):
+  a = a.lower()
+  b = b.lower()
+  a_len = len(a)
+  b_len = len(b)
+  
+  if a_len == b_len:
+    if a == b:
+      return True
+    return False
+    
+  if a_len > b_len:
+    if a[a_len - b_len:] == b:
+      return True
+    return False
+  else:
+    if b[b_len - a_len:] == a:
+      return True
+    return False
